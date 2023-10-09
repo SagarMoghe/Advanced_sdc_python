18efd46dcd8548a87c6274bd241bbe7e1700b4c2
Sagar Moghe <16164018+SagarMoghe@users.noreply.github.com>
2023-10-09 08:07:48 +0530

    completed mysql_datafactory (#6)
    * completed mysql_datafactory
    
    * modified github actions
    
    * modified github actions
    
    * modified github actions
    
    * modified github actions

---
+++ src/leagueObjectModel/player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, name: str):
+        self.id = id(self)
+        self.name = name
+        print(f'player {self.name} created with id {self.id}')
+
+
+# Player("sagar")
